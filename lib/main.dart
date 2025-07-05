a313300e079458f02fe5f09bb983529e65c60e1b
Burak Gençer <bggithub@proton.me>
2025-07-05 12:21:02 +0300

    added button for random color selection, added rgb for color information, added square or circle box, added toast notification, added popupmenu, added show and hide color name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -22,10 +25,42 @@ class ColorPickerPage extends StatefulWidget {
 
 class _ColorPickerPageState extends State<ColorPickerPage> {
   Color selectedColor = Colors.blue;
+  bool isCircular = false;
+  bool isShowColorName = true;
+
+  void _rastgeleRenkSec() {
+    final colors = renkler.keys.toList();
+    final rastgeleSayi = Random().nextInt(colors.length);
+    final randomColor = colors[Random().nextInt(colors.length)];
+    setState(() {
+      selectedColor = randomColor;
+      debugPrint(rastgeleSayi.toString());
+    });
+  }
+
+  void _renginKodunuGoster() {
+    Fluttertoast.showToast(
+      msg:
+          "RGB : (${selectedColor.red}, ${selectedColor.green}, ${selectedColor.blue})",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: selectedColor,
+      textColor: Colors.white,
+      fontSize: 24.0,
+    );
+  }
+
+  void _containerSekliniDegistir() {
+    setState(() {
+      isCircular = !isCircular;
+    });
+  }
+
   final Map<Color, String> renkler = {
     Colors.red: 'Kırmızı',
     Colors.blue: 'Mavi',
-    Colors.green: 'Yeşil',
+    const Color.fromARGB(255, 224, 255, 225): 'Yeşil',
     Colors.yellow: 'Sarı',
     Colors.orange: 'Turuncu',
     Colors.purple: 'Mor',
@@ -47,7 +82,44 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Renk Seçici'), centerTitle: true),
+      appBar: AppBar(
+        title: Text('Renk Seçici'),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              setState(() {
+                isShowColorName = !isShowColorName;
+              });
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: "a",
+                  child: Row(
+                    children: [
+                      Icon(
+                        isShowColorName
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        size: 20,
+                        color: Colors.black54,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        isShowColorName
+                            ? "Renk Adını Gizle"
+                            : "Renk Adını Göster",
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
@@ -56,7 +128,7 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
               height: 200,
               decoration: BoxDecoration(
                 color: selectedColor,
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(isCircular ? 100 : 10),
                 boxShadow: [
                   BoxShadow(
                     color: selectedColor.withOpacity(0.5),
@@ -67,36 +139,59 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
               ),
             ),
             SizedBox(height: 10),
-            Text("Mavi"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DropdownButton<Color>(
-                  value: selectedColor,
-                  items:
-                      renkler.entries.map((entry) {
-                        return DropdownMenuItem(
-                          value: entry.key,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 20,
-                                height: 20,
-                                color: entry.key,
-                              ),
-                              SizedBox(width: 4),
-                              Text(entry.value),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedColor = value!;
-                    });
-                  },
-                ),
-              ],
+            isShowColorName
+                ? Text(renkler[selectedColor] ?? 'Seçilen Renk')
+                : SizedBox(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DropdownButton<Color>(
+                    value: selectedColor,
+                    items:
+                        renkler.entries.map((entry) {
+                          return DropdownMenuItem(
+                            value: entry.key,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 20,
+                                  height: 20,
+                                  color: entry.key,
+                                ),
+                                SizedBox(width: 4),
+                                Text(entry.value),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedColor = value!;
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: _rastgeleRenkSec,
+                    child: Text("Rastgele"),
+                  ),
+                  IconButton(
+                    onPressed: _renginKodunuGoster,
+                    icon: Icon(Icons.info),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _containerSekliniDegistir();
+                    },
+                    icon: Icon(
+                      isCircular
+                          ? Icons.square_outlined
+                          : Icons.circle_outlined,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
